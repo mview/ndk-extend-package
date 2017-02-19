7895f3ccd59abd5577417ec4ba17a83fd7a57264
Ye Joey <joeyye@foxmail.com>
2017-02-19 21:57:20 +0800

    remove absolute path for ndk

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,10 @@
 if [ -n "$DIR" ]; then
-cd $DIR
-~/android-ndk-r10e/ndk-build V=$DEBUG ANDROID_VER=$VER NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk -j8
+    cd $DIR
+    NDKBUILD_PATH=`which ndk-build`
+    if [ $? -eq 0 ]; then
+	NDK_HOME=`dirname $NDKBUILD_PATH`
+	ndk-build NDK_HOME=$NDK_HOME V=$DEBUG ANDROID_VER=$VER NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk -j8
+    else
+	echo "add NDK path to search path, e.g. export PAHT=/NDK path:\$PATH"
+    fi
 fi
