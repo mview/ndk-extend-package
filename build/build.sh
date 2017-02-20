d381de7dc6b77a2ca0dfcc43bb952c24cf6e4674
Ye Joey <joeyye@foxmail.com>
2017-02-20 13:44:06 +0800

    typo error

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,6 @@ if [ -n "$DIR" ]; then
 	NDK_HOME=`dirname $NDKBUILD_PATH`
 	ndk-build NDK_HOME=$NDK_HOME V=$DEBUG ANDROID_VER=$VER NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk -j8
     else
-	echo "add NDK path to search path, e.g. export PAHT=/NDK path:\$PATH"
+	echo "add NDK path to search path, e.g. export PATH=~/<NDK path>:\$PATH"
     fi
 fi
